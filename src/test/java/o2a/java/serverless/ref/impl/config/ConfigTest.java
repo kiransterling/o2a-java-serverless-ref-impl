f09d1ef6d555275d2695eb20a19b0e8ebf9f0119
Bathalu Shivakumar <Vinay.Shivakumar@team.telstra.com>
2020-08-10 21:25:50 +0530

    Changes for testing dynamoDB

---
+++ src/test/java/o2a/java/serverless/ref/impl/config/ConfigTest.java
@@ -0,0 +1,30 @@
+package o2a.java.serverless.ref.impl.config;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.IDynamoDBMapper;
+
+import cloud.localstack.TestUtils;
+import cloud.localstack.docker.annotation.LocalstackDockerProperties;
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Primary;
+
+@Factory
+@LocalstackDockerProperties(services = { "dynamodb" })
+public class ConfigTest {
+
+	@Primary
+	@Bean
+	public IDynamoDBMapper mapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+		return new DynamoDBMapper(amazonDynamoDB, config);
+	}
+
+	@Primary
+	@Bean
+	public AmazonDynamoDB client() {
+		return TestUtils.getClientDynamoDB();
+	}
+
+}
